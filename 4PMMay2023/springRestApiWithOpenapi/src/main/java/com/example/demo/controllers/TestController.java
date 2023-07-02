092614cb9df7b6fdd0cc05884323d529dacf0d7b
Kabiraj Shrestha <rajkabee@gmail.com>
2023-07-02 14:45:42 +0545

    main

---
+++ 4PMMay2023/springRestApiWithOpenapi/src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.security.MyUserDetailsService;
+
+
+
+
+@RestController
+public class TestController {
+	@Autowired
+	MyUserDetailsService myUserDetailsService;
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "this is home api called";
+	}
+
+	@RequestMapping("/user")
+	public String user() {
+		return "this is user api called";
+	}
+	
+	@RequestMapping("/admin")
+	public String admin() {
+		return "this is admin api called";
+	}
+	@RequestMapping("/addUsers")
+	public String addUsers() {
+		myUserDetailsService.addUsers();
+		return "users added to the database";
+	}
+	
+}
